c9dff81e78bd64218b89bf398037c1027179dbed
SByte <stormybytes@gmail.com>
2025-12-08 22:33:18 +0700

    2025d08

---
+++ 2025/day08/src/p2.rs
@@ -0,0 +1,141 @@
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap};
+
+use aocutils::cartes::dim3::vec::Vec3;
+
+#[derive(Eq, PartialEq, Debug)]
+struct DistSort {
+    dist: i64,
+    v1: Vec3<i64>,
+    v2: Vec3<i64>,
+}
+impl Ord for DistSort {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.dist.cmp(&other.dist)
+    }
+}
+impl PartialOrd for DistSort {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Eq, PartialEq)]
+struct Point {
+    p: Vec3<i64>,
+    circuit: Option<usize>,
+}
+
+pub fn part2(input: &str) -> i64 {
+    let num_points = input.lines().count();
+    let mut points: HashMap<Vec3<i64>, Point> = HashMap::with_capacity(num_points);
+    let mut distances = BinaryHeap::new();
+    for line in input.lines() {
+        let (left, line) = line.split_once(',').unwrap();
+        let (middle, right) = line.split_once(',').unwrap();
+        let (left, middle, right) = (
+            left.parse::<i64>().unwrap(),
+            middle.parse::<i64>().unwrap(),
+            right.parse::<i64>().unwrap(),
+        );
+
+        let v = Vec3::from((left, middle, right));
+        for v_other in points.keys() {
+            let v_other: Vec3<i64> = *v_other;
+            let dist = v_other.euclidian_dist_sq(v);
+            distances.push(Reverse(DistSort {
+                dist,
+                v1: v,
+                v2: v_other,
+            }));
+        }
+
+        points.insert(
+            v,
+            Point {
+                p: v,
+                circuit: None,
+            },
+        );
+    }
+    let mut circuits = vec![];
+    while let Some(dist) = distances.pop() {
+        let dist = dist.0;
+        let c1_some = points[&dist.v1].circuit.is_some();
+        let c2_some = points[&dist.v2].circuit.is_some();
+        match (c1_some, c2_some) {
+            (false, false) => {
+                let circ_idx = circuits.len();
+                points.get_mut(&dist.v1).unwrap().circuit = Some(circ_idx);
+                points.get_mut(&dist.v2).unwrap().circuit = Some(circ_idx);
+                circuits.push(vec![dist.v1, dist.v2]);
+                // impossible for these to be the final two junctions joined as they just created a
+                // new circuit
+            }
+            (true, false) => {
+                let circ = points[&dist.v1].circuit.unwrap();
+                points.get_mut(&dist.v2).unwrap().circuit = Some(circ);
+                circuits[circ].push(dist.v2);
+                if circuits[circ].len() == num_points {
+                    return points[&dist.v1].p.0 * points[&dist.v2].p.0;
+                }
+            }
+            (false, true) => {
+                let circ = points[&dist.v2].circuit.unwrap();
+                points.get_mut(&dist.v1).unwrap().circuit = Some(circ);
+                circuits[circ].push(dist.v1);
+                if circuits[circ].len() == num_points {
+                    return points[&dist.v1].p.0 * points[&dist.v2].p.0;
+                }
+            }
+            (true, true) => {
+                // Move all points in circuit 2 into circuit 1
+                let circ1_idx = points[&dist.v1].circuit.unwrap();
+                let circ2_idx = points[&dist.v2].circuit.unwrap();
+                if circ1_idx == circ2_idx {
+                    continue;
+                }
+                for c in &circuits[points[&dist.v2].circuit.unwrap()] {
+                    points.get_mut(c).unwrap().circuit = Some(circ1_idx);
+                }
+                let [circ1, circ2] = circuits.get_disjoint_mut([circ1_idx, circ2_idx]).unwrap();
+                circ1.append(circ2);
+                if circ1.len() == num_points {
+                    return points[&dist.v1].p.0 * points[&dist.v2].p.0;
+                }
+            }
+        }
+    }
+
+    unreachable!("Junctions did not connect into one big circuit!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        let input = "162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689";
+        assert_eq!(part2(input), 25272);
+    }
+}
